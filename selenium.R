509b55072adfb53acabee336e119be6555c4c105
favstats <fabio.votta@gmail.com>
2023-10-21 18:39:42 +0200

    sunset

--- selenium.R
+++ selenium.R
@@ -176,7 +176,7 @@ retrieve_spend_daily <- function(id, the_date, cntry = sets$cntry) {
 }
 
 # debugonce(retrieve_spend_daily)
-# retrieve_spend_daily <- possibly(retrieve_spend_daily, otherwise = NULL, quiet = F)
+retrieve_spend_daily <- possibly(retrieve_spend_daily, otherwise = NULL, quiet = F)
 
 # daily_spending <- readRDS("data/daily_spending.rds")
 # Apr 17, 2023 - May 16, 2023
